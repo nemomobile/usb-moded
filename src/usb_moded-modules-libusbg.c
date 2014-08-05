03d63005e2c5360c8d1d6b759e27b2b60a7393c3
Philippe De Swert <philippe.deswert@jollamobile.com>
2014-08-05 10:21:46 +0300

    Start adding libusbg support
    Signed-off-by: Philippe De Swert <philippe.deswert@jollamobile.com>

---
+++ src/usb_moded-modules-libusbg.c
@@ -0,0 +1,115 @@
+/**
+  @file usb_moded-modules-libusbg.c
+ 
+  Copyright (C) 2014 Jolla. All rights reserved.
+
+  @author: Philippe De Swert <philippe.deswert@jolla.com>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the Lesser GNU General Public License 
+  version 2 as published by the Free Software Foundation. 
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+ 
+  You should have received a copy of the Lesser GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+  02110-1301 USA
+*/
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+#include <glib.h>
+
+#include <libkmod.h>
+
+#include "usb_moded.h"
+#include "usb_moded-modules.h"
+#include "usb_moded-log.h"
+#include "usb_moded-config.h"
+#include "usb_moded-dbus.h"
+#include "usb_moded-dbus-private.h"
+#include "usb_moded-config.h"
+#include "usb_moded-modesetting.h"
+
+
+/** load module 
+ *
+ * @param module Name of the module to load
+ * or in this case tree to set up and activate
+ * @return 0 on success, non-zero on failure
+ *
+ */
+int usb_moded_load_module(const char *module)
+{
+}
+
+/** unload module
+ *  
+ * @param module Name of the module to unload
+ * or in this case deactivate the gadget
+ * @return 0 on success, non-zero on failure
+ *
+ */
+int usb_moded_unload_module(const char *module)
+{
+}
+
+/** Check which state a module is in
+ * or in this case check what state the gadgetfs tree is
+ *
+ * @return 1 if loaded, 0 when not loaded
+ */
+inline static int module_state_check(const char *module)
+{
+	return(0);
+}
+
+/** find which module is loaded 
+ *
+ * @return The name of the loaded module, or NULL if no modules are loaded.
+ */
+inline const char * usb_moded_find_module(void)
+{
+  return(0);
+}
+
+/** clean up for modules when usb gets disconnected
+ *
+ * @param module The name of the module to unload
+ * @return 0 on success, non-zero on failure
+ *
+ */
+inline int usb_moded_module_cleanup(const char *module)
+{
+	return(0);
+}
+
+/** try to unload modules to support switching
+ *
+ *
+ * @param force force unloading with a nasty clean-up on TRUE, or just try unloading when FALSE
+ * @return 0 on success, 1 on failure, 2 if hard clean-up failed
+ */
+inline int usb_moded_module_switch_prepare (int force)
+{
+	return 0;
+}
+
+/** check for loaded modules and clean-up if they are not for the chosen mode 
+ *
+ * @param module_name  module name to check for
+ *
+ */
+inline void check_module_state(const char *module_name)
+{
+  return;
+}
+
+
